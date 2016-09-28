8f5527efd13e9ceea67e49e78931ba08156cf1b2
Jerome Schalkwijk <jeromeschalkwijk@gmail.com>
2016-09-28 19:09:49 +0100

    Project set-up

---
+++ KaboomBoy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KaboomBoy
+//
+//  Created by J. Schalkwijk on 28/09/2016.
+//  Copyright © 2016 JeromeSchalkwijk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
